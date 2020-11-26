557b48fdf844d56f1de44c1b6d503cd3d860510d
Jon Lachlan <jonlachlan@gmail.com>
2020-11-26 14:23:02 -0800

    Update browser example to handle websocket Text messages and multi-chunk Binary messages

--- browser/main.js
+++ browser/main.js
@@ -32,29 +32,60 @@
     
     updatesWebsocket.onmessage = async function (event) {
         const decoder = new TextDecoder('utf-8');
-        if(event.data.stream) {
+        if(event.data?.stream) {
 
-            const dataStream = event.data.stream();
-            const reader = dataStream.getReader();
+            const dataStream = 
+                event.data.stream();
+            const reader = 
+                dataStream.getReader();
         
-            const dataAsyncIterator = {
-                next: async () => {
-                    return await reader.read();
-                },
-                [Symbol.asyncIterator]: function() {
-                    return this;
-                }
-            };
+            const dataAsyncIterator = 
+                {
+                    next: async () => {
+                        return await reader.read();
+                    },
+                    [Symbol.asyncIterator]: function() {
+                        return this;
+                    }
+                };
     
-            let payload;
+            const payloadArray = 
+                [];
+            let payloadLength = 
+                0;
             // breaks when finished
             for await (const chunk of dataAsyncIterator) {
-                payload = chunk;
+                payloadArray.push(chunk);
+                payloadLength += 
+                    chunk.length;
             }
-            console.log(`received websocket payload with length ${payload.length}`);
+            console.log(`received websocket payload with length ${payloadLength}`);
             
+            const payload = 
+                new Uint8Array(payloadLength);
+            
+            payloadArray.reduce(
+                (offset, chunk) => {
+                    payload.set(
+                        chunk,
+                        offset
+                    );
+                    offset += 
+                        chunk.length;
+                }, 
+                0 /* initialValue */
+            );
             updatesWebsocket.send(
                 payload
+            );
+        } else if(event.data) {
+            console.log(`received websocket payload ${event.data}`);
+            updatesWebsocket.send(
+                event.data
+            );
+        } else {
+            console.warn(
+                'event.data of websocket event is not defined'
             );
         }
     }
